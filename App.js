955e5580feb7035f72612679b25389b12c1258fa
kanokphon singphu <88051146+oatsan@users.noreply.github.com>
2023-09-02 10:14:57 +0700

    First Commit [Lecture9-September-02-2023]

--- App.js
+++ App.js
@@ -1,44 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { View, Text , Button} from 'react-native'
 import React from 'react'
-import TouchableExample from './components/TouchableExample'
-import TouchablePractice from './components/TouchablePractice'
-import Example_useEffect from './components/Example_useEffect'
-import UseEffectFlatList from './components/UseEffectFlatList'
-import RandomUsersScreen from './components/RandomUsersScreen'
-import FlatList_Example1 from './components/FlatList_Example1'
-import FlatList_HeaderFooter from './components/FlatList_HeaderFooter'
-import FlastListAPI from './components/FlastListAPI'
-import News from './components/News'
-import ProductScreen from './components/ProductScreen'
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import HomeScreen from './components/screens/HomeScreen'
+import DetailsScreen from './components/screens/DetailsScreen'
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      {/* <TouchableExample/>  */}
-       {/* <TouchablePractice/>  */}
-       {/* <Example_useEffect/> */}
-       {/* <UseEffectFlatList/> */}
-       {/* <RandomUsersScreen/> */}
-       {/* <FlatList_Example1/> */}
-       {/* <FlatList_HeaderFooter/> */}
-       {/* <FlastListAPI/> */}
-       {/* <News/> */}
-       <ProductScreen/>
-       
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen 
+        name='Home'
+        component={HomeScreen}
+        options={{title: 'Overview'}}
+        />
+        <Stack.Screen 
+        name='Details' 
+        component={DetailsScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
 export default App
-
-const styles = StyleSheet.create({
-
-  container:{
-
-    flex:1,  
-
-    justifyContent:'center',    
-
-
- }
-}) 
---
+++ App2.js
@@ -0,0 +1,44 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import TouchableExample from './components/TouchableExample'
+import TouchablePractice from './components/TouchablePractice'
+import Example_useEffect from './components/Example_useEffect'
+import UseEffectFlatList from './components/UseEffectFlatList'
+import RandomUsersScreen from './components/RandomUsersScreen'
+import FlatList_Example1 from './components/FlatList_Example1'
+import FlatList_HeaderFooter from './components/FlatList_HeaderFooter'
+import FlastListAPI from './components/FlastListAPI'
+import News from './components/News'
+import ProductScreen from './components/ProductScreen'
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      {/* <TouchableExample/>  */}
+       {/* <TouchablePractice/>  */}
+       {/* <Example_useEffect/> */}
+       {/* <UseEffectFlatList/> */}
+       {/* <RandomUsersScreen/> */}
+       {/* <FlatList_Example1/> */}
+       {/* <FlatList_HeaderFooter/> */}
+       {/* <FlastListAPI/> */}
+       {/* <News/> */}
+       <ProductScreen/>
+       
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+
+  container:{
+
+    flex:1,  
+
+    justifyContent:'center',    
+
+
+ }
+}) 
